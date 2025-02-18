62aee13e556247f412fd73e1573e29e3fb4c3973
Ivan-Skorokhodov <dreikonline5554545@gmail.com>
2025-02-18 15:51:43 +0300

    update gitignore

---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+gnome-terminal -- bash -c "./client.py; exec bash"
+gnome-terminal -- bash -c "./client2.py; exec bash"
+gnome-terminal -- bash -c "cd WebServer && ./server.py; exec bash"
